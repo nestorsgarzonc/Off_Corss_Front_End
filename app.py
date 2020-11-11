5cdfbfcabc30baecba175831361521ad0e68f243
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-11-10 22:21:24 -0500

    req

--- app.py
+++ app.py
@@ -899,4 +899,4 @@ def process_form(start_hour, start_minute, end_hour, end_min, cam, date):
 
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port='8050', debug=True)
+    app.run_server(host='0.0.0.0', port='80', debug=True)
